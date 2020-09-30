2abad66b478bc2b869e8bcc41934a78585462faf
Louis Savoie <thelousavoie@gmail.com>
2020-09-29 21:28:36 -0400

    add toplink, rename navbar.js to script.js

--- javascript/script.js
+++ javascript/script.js
@@ -2,25 +2,36 @@
 const toggler = document.querySelector('#nav-toggler');
 const nav = document.querySelector('nav');
 const navbar = document.querySelector('header');
+const toplink = document.querySelector('#toplink');
 
-// TOGGLE FUNCTION
+// NAV TOGGLE
 function toggleNav() {
     nav.classList.toggle('nav-active');
 }
 
-// CALL TOGGLE ON CLICK
+// CALL NAV TOGGLE ON CLICK
 toggler.addEventListener('click', function() {
     toggleNav();
 });
 
-// CHANGE BACKGROUND ON SCROLL
+// ON SCROLL EVENT
 window.onscroll = function () {
+    // SCROLL DOWN FROM TOP
     if (document.body.scrollTop >= 70  || document.documentElement.scrollTop >= 70) {
+        // NAVBAR BACKGROUND
         navbar.classList.add("nav-colored");
         navbar.classList.remove("nav-transparent");
+        // TOPLINK DISPLAY
+        toplink.classList.remove("unscrolled");
+        toplink.classList.add("scrolled");
     } 
+    // SCROLL TO TOP
     else {
+        // NAVBAR NO BACKGROUND
         navbar.classList.add("nav-transparent");
         navbar.classList.remove("nav-colored");
+        // TOPLINK DISPLAY NONE
+        toplink.classList.add("unscrolled");
+        toplink.classList.remove("scrolled");
     }
 };
